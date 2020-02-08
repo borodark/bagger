f5280c306eec2b503217d5738e33e99f0ad3edb5
Igor O'Sten <borodark@gmail.com>
2020-02-08 18:59:36 -0500

    dataset

--- 3class-dataset.r
+++ 3class-dataset.r
@@ -14,7 +14,11 @@ write.csv(out, file="1000-6D-2.csv", row.names=F)
 # [a,b,c] = [-0.2, 0.77004,0.86541  ]
 #
 #
-r <- read.csv(file = "1000-2D-2.csv")
+r1 <- read.csv(file = "1000-2D-2-x1x2.csv", col.names = c('X1','X2'))
+which <- read.csv(file = "1000-2D-2-y.csv", col.names = 'which')
+r <- cbind(r1,which)
+#r['which'] = which
+#r <- read.csv(file = "1000-2D-2.csv")
 # for -0.2 + 0.77004 * x1 + 0.86541 *x2 = 0
 # for a + b * x1 + c *x2 = 0
 # a+b*x1 = -c * x2
@@ -27,8 +31,8 @@ c <- 0.75637
 eq = function(x){a/(-c) + (b/-c)*x }
 par(pch=22, col="red") # plotting symbol and color
 par(mfrow=c(1,1)) # all plots on one page
-
-with(r, { 
+# plot 
+with(r,{ 
   curve(eq, -5, 5, xlab = 'X1', ylab = 'X2')
   points(x=X1, y=X2, pch=20, col = which +1)
 })
